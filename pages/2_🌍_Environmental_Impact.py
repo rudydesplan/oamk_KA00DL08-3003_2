6d8794c898ef1cdf22de6d60fa1a9b947763c4e8
rudy desplan <desrudy@hotmail.com>
2025-02-26 17:04:46 +0100

    Update 2_🌍_Environmental_Impact.py

--- pages/2_🌍_Environmental_Impact.py
+++ pages/2_🌍_Environmental_Impact.py
@@ -22,7 +22,7 @@ def main():
     st.header("Environmental Impact vs Carbon Footprint")
     
     fig = px.scatter(
-        df,  # Sample for better performance
+        df.sample(30000, random_state=42),  # Sample for better performance
         x='environmental_score_score',
         y='co2_total',
         color='nutriscore_score',
