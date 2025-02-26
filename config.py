198b9af315312cbd45f620e3fdaa4647751a08c8
rudy desplan <desrudy@hotmail.com>
2025-02-26 17:54:48 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+import plotly.express as px
+
+COLOR_SCALE = px.colors.sequential.Viridis
+NUTRISCORE_COLORS = ['#038C3E', '#8DBF6B', '#F2CB05', '#F29F05', '#D95204']
+ENV_COLORS = px.colors.sequential.thermal
